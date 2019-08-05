23f680c8b693657fe49e24e7424683ffc6beafc2
kwamitchamounelson <kwamitchamounelson@gmail.com>
2019-08-05 08:59:24 +0100

    chercher le mot

--- chercher le mot nelson/findWord/app/src/main/java/com/example/findword/entities/Caracter.kt
+++
@@ -1,5 +0,0 @@
-package com.example.findword.entities
-
-data class Caracter(var char: Char,var x:Int,var y:Int) {
-    constructor():this(' ',0,0)
-}
--- chercher le mot nelson/findWord/app/src/main/java/com/example/findword/entities/Word.kt
+++
@@ -1,5 +0,0 @@
-package com.example.findword.entities
-
-data class Word(var text:String,var isValid:Boolean?,var caracters: ArrayList<Caracter>?,var definition:String?) {
-    constructor():this("",false, arrayListOf(),"")
-}
